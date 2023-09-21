daa23d750a7431c740dd0d341b25c29aa810a378
a1bod <141325010+a1bod@users.noreply.github.com>
2023-09-21 16:23:09 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -76,14 +76,14 @@ def gen_user(choice):
     if choice == "3":
         c = d = random.choices(a)
         d = random.choices(b)
-        f = [c[0], d[0], c[0], c[0], "bot"]
+        f = [c[0], d[0], c[0], c[0],]
         random.shuffle(f)
         username = ''.join(f)
         username = username+'bot'
         if username in banned[0]:
             c = d = random.choices(a)
             d = random.choices(b)
-            f = [c[0], d[0], c[0], c[0], "bot"]
+            f = [c[0], d[0], c[0], c[0]]
             random.shuffle(f)
             username = ''.join(f)
             username = username+'bot'
